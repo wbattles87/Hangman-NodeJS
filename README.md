f004e21dc7ed6d3998a77354232a569b9c97e0ed
Ellen Bankhead <wbattles87@yahoo.com>
2018-02-10 10:43:15 -0600

    Fixed some merge conflicts, added an updated readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Node-Hangman
+
+Welcome to the Node Js hangman game.  The point of this game was to improve my javascript skills and implement my earlier version of a hangman game but deploy it completely in the terminal.
+
+The only dependency used for this project was inquirer.
+
+First, the main.js file is the main component of this program.  It requires all the other files to run the game.  The game.js file 
+is where my current word bank is hidden.  The letter.js file is where the word blanks are filled in for both the correct guessed and wrongly guessed letters for the chosen word.  Finally, word.js is where we compare the users input word against the correct word and tell them if they are correct or not.
+
+Below are two screen shots showing the working node js program in action.
+
+1) First Portion of the working app.
+![alt text](images/pic1.png)
+
+2) Second Pic of the working program
+
+![alt text](images/pic2.png)
+
+
--- letter.js
+++ letter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Pulling in the main js file with the random word generator
 var main = require('./main.js');
 exports.guessArr = [];
@@ -31,38 +30,4 @@ exports.displayWord = function () {
 //Function to make changes to the guessArray when a correct letter is guessed. 
 exports.editArray = function (position, letter) {
     exports.guessArr[position] = letter;
-=======
-// Pulling in the main js file with the random word generator
-var main = require('./main.js');
-exports.guessArr = [];
-var guessDisplay = "";
-exports.wordArr = [];
-
-//Separates the wordChoice into an array for easier accessibility.
-exports.initDisplay = function () {
-    var wordChoice = main.chosenWord;
-    for (var i = 0; i < wordChoice.length; i++) {
-        exports.wordArr.push(wordChoice.charAt(i));
-        if (wordChoice.charAt(i) == '\xa0') {
-            exports.guessArr.push('\xa0');
-        }
-        else {
-            exports.guessArr.push('_');
-        }
-    };
-}
-
-//Displays what the user has guessed/blanks in the word file. 
-exports.displayWord = function () {
-    guessDisplay = "";
-    for (var i = 0; i < exports.guessArr.length; i++) {
-        guessDisplay += exports.guessArr[i] + " ";
-    }
-    console.log(guessDisplay);
-};
-
-//Function to make changes to the guessArray when a correct letter is guessed. 
-exports.editArray = function (position, letter) {
-    exports.guessArr[position] = letter;
->>>>>>> a7910310891b4fefb6446813e0bb231fd1bc3d6d
 };
